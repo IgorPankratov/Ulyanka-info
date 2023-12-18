a86c39708fc74154e08fe0aab19dc4b011c6fa60
Igor <ipankrat85@gmail.com>
2023-12-18 22:02:36 +0300

    добавление файлов в репозиторий github

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+    let mySource = myImage.getAttribute("src");
+    if (mySource === 'images/ul_panorama.jpg') {
+        myImage.setAttribute('src', "images/ul_petrograd_1916.jpg");
+    } else {
+        myImage.setAttribute('src', 'images/ul_panorama.jpg');
+    }
+};
+
+let myButton = document.querySelector('button');
+let myHeader = document.querySelector('h1');
+
+function SetUserName() {
+    let myName = prompt('Введите пожалуйста своё имя');
+    localStorage.setItem('name', myName);
+    myHeader.textContent = 'Ульянка приветствует тебя' + myName;
+}
+
+if (!localStorage.getItem('name')) {
+    SetUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeader.textContent = 'Ульянка приветствует тебя, ' + storedName;
+}
+
+myButton.onclick = function () {
+    SetUserName();
+}
+
